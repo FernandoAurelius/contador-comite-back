407302cbd1eaa4164bd7ed028da04193c2a4d555
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-03-23 20:42:36 -0300

    initial commit

---
+++ src/main/java/br/com/floresdev/contador_comite_back/ContadorComiteBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.floresdev.contador_comite_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorComiteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorComiteBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/floresdev/contador_comite_back/ContadorComiteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.floresdev.contador_comite_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorComiteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
