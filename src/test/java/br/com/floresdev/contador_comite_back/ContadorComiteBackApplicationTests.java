54da0ea7f5b7fb01df0e3affe98435755b3c89e1
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-04-01 10:37:00 -0300

    test: add SpringBootTest annotation with properties to load context

--- src/test/java/br/com/floresdev/contador_comite_back/ContadorComiteBackApplicationTests.java
+++ src/test/java/br/com/floresdev/contador_comite_back/ContadorComiteBackApplicationTests.java
@@ -3,11 +3,13 @@ package br.com.floresdev.contador_comite_back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "api.security.token.secret=7cdlcqSgFITok+LwWCsXIVWVJojeR78G/Yz9pKlunRU="
+})
 class ContadorComiteBackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
