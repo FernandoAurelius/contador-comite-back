a101a2505bb88d6840e81e1824e552cad7de94f5
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-04-03 09:22:35 -0300

    feat: implement user authentication and authorization
    This commit introduces several new features and fixes related to user
    authentication and authorization:
    
    - Added a new controller `UserController` to handle user-related
      requests, including fetching the current user's information.
    - Implemented the `getCurrentUser` endpoint in `UserController` to
      retrieve the current user based on the JWT token stored in the
      cookies.
    - Created a `HttpServletResquest` (it seems like a typo) class.
    - Modified the `UserDTO` class to include `toEntity` and
      `fromEntity` methods for converting between the DTO and entity
      objects.
    - Updated the `SecurityFilter` class to include a static method for
      recovering the JWT token from the cookies.
    - Implemented the `getUserByToken` method in the `UserService` class
      to retrieve a user based on the JWT token.
    - Changed the exception type caught in `AuthenticationController` from
      `Exception` to `AuthenticationException` for better specificity.

--- src/main/java/br/com/floresdev/contador_comite_back/services/UserService.java
+++ src/main/java/br/com/floresdev/contador_comite_back/services/UserService.java
@@ -4,11 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.floresdev.contador_comite_back.domain.repositories.UserRepository;
+import br.com.floresdev.contador_comite_back.domain.user.User;
+import br.com.floresdev.contador_comite_back.infra.SecurityFilter;
+import br.com.floresdev.contador_comite_back.infra.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private TokenService service;
+
+    public User getUserByEmail(String email) {
+        var userOpt = repository.findByEmail(email);
+
+        if (userOpt.isPresent()) return (User) userOpt.get();
+
+        return null;
+    }
+
+    public User getUserByToken(HttpServletRequest request) {
+        var token = SecurityFilter.recoverTokenFromCookie(request);
+        var email = service.validateToken(token);
+        return this.getUserByEmail(email);
+    }
 
 }
