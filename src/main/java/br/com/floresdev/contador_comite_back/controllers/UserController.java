5d55cc686693c720d580cfdab2b395220e6469ea
FernandoAurelius <miguelfernandoaurelius@gmail.com>
2025-04-03 10:45:49 -0300

    feat(UserController): implement GET /me endpoint to retrieve current user
    This commit introduces a new endpoint, GET /me, in the UserController.
    This endpoint retrieves the current user's information based on the
    token provided in the request. It returns a UserDTO object containing
    the user's details if the user is found. If the user is not found
    (token is invalid or user does not exist), it returns a 401
    Unauthorized status code.
    
    The commit also includes Swagger annotations to document the endpoint's
    functionality, request parameters, and possible responses.

--- src/main/java/br/com/floresdev/contador_comite_back/controllers/UserController.java
+++ src/main/java/br/com/floresdev/contador_comite_back/controllers/UserController.java
@@ -1,6 +1,7 @@
 package br.com.floresdev.contador_comite_back.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.floresdev.contador_comite_back.domain.user.User;
 import br.com.floresdev.contador_comite_back.domain.user.dto.UserDTO;
 import br.com.floresdev.contador_comite_back.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -19,10 +25,15 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Operation(summary = "Dados do usuário atual", description = "Obtém informações do usuário com base no token recebido")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Retorna o usuário com base na sessão atual", content = @Content(schema = @Schema(implementation = UserDTO.class))),
+        @ApiResponse(responseCode = "", description = "Usuário não encontrado na sessão atual", content = @Content)
+    })
     @GetMapping("/me")
     public ResponseEntity<UserDTO> getCurrentUser(HttpServletRequest request, HttpServletResponse response) {
         User user = service.getUserByToken(request);
-        if (user == null) return ResponseEntity.notFound().build();
+        if (user == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         return ResponseEntity.ok(UserDTO.fromEntity(user));
     }
 
